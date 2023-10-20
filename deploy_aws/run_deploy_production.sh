c8612e5255855cdd890de4915fbb9e925ee85e45
Daniel Salvatori <danielfelipesalvatori@gmail.com>
2023-10-20 11:40:47 -0300

    change .sh

--- deploy_aws/run_deploy_production.sh
+++ deploy_aws/run_deploy_production.sh
@@ -2,7 +2,7 @@
 
 data=$(date +"%d_%m_%Y_%H_%M_%S")
 
-path='/home/ubuntu/insiders_clusterization'
-path_to_envs='/home/ubuntu/.pyenv/versions/insiders_clusterization/bin'
+path='/home/ubuntu/deploy_aws'
+path_to_envs='/home/ubuntu/.pyenv/versions/3.10.4/envs/insidersclustering/bin'
 
 $path_to_envs/papermill $path/deploy_aws.ipynb $path/notebook-deploy_aws_$data.ipynb
